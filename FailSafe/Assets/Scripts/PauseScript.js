04e4a06044c2f97a019b7fc2c7b7d0c1ff934b46
LIVINGSTONE <slivin15@LAB-M627-Mac-15.local>
2013-02-15 15:43:05 +0000

    15-2-13 V2

--- FailSafe/Assets/Scripts/PauseScript.js
+++ FailSafe/Assets/Scripts/PauseScript.js
@@ -3,6 +3,13 @@
 
 var gamePaused: boolean;
 
+public var pauseLocation: GameObject;
+public var pauseMenu: GameObject;
+
+private var pauseScreen: GameObject;
+
+public var Resume: GameObject;
+
 
 function Start () 
 {
@@ -11,17 +18,39 @@ function Start ()
 
 function Update () 
 {
-	if(Input.GetMouseButtonDown(0) && gamePaused == false)
-	{
-		Time.timeScale = 0.0;
-		gamePaused = true;
-		Debug.Log("paused");	
+	if(Input.GetMouseButtonDown(0))
+	{	
+		
+		var pauseClick: int = 1;		
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		var hit : RaycastHit;	
+		
+		if(gamePaused == false && pauseClick == 1)
+		{
+			Time.timeScale = 0.0;
+			gamePaused = true;
+			pauseClick = 0;
+			Debug.Log("paused");
+			pauseScreen = GameObject.Instantiate(pauseMenu, pauseLocation.transform.position, pauseLocation.transform.rotation);
+			Resume = GameObject.Find("Resume");
+		}
+		
+		
+		if (Physics.Raycast(ray, hit, 100.0))
+		{	
+					
+				if(gamePaused == true && pauseClick == 1 && hit.collider.gameObject == Resume)
+				{
+					Time.timeScale = 1.0;
+					gamePaused = false;
+					pauseClick = 0;			
+					Debug.Log("unpaused");
+					Destroy(pauseScreen);
+				}		
+		}
+		
 	}
-	else if(Input.GetMouseButtonDown(0) && gamePaused == true)
-	{
-		Time.timeScale = 1.0;
-		gamePaused = false;
-		Debug.Log("resumed");	
-	}
+		
+	
 	
 }
